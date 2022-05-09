7b91794dbfabca270976a99917f7633689b2f24a
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-05-09 17:11:39 +0530

    credit-card-service

---
+++ credit-card-service/src/main/java/com/training/CreditCardService.java
@@ -0,0 +1,23 @@
+package com.training;
+
+public class CreditCardService {
+	
+	CreditCard card = new CreditCard(1234,"AAA",500);
+	
+	public boolean addCard(CreditCard card) {
+		
+		int i =0;
+		
+		CreditCard[] card1= new  CreditCard[2];
+		
+		
+		return true;
+	}
+	
+	public CreditCard[] getCards() {
+		
+		return getCards();
+	}
+	
+
+}
