f9c08b1915bb21c03e8314096e8b6ad9ebe0d618
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-05-24 10:06:27 +0530

    eureka-client-one

---
+++ eureka-client-one/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+	
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping(path = "/books")
+	public List<String>getBooks(){
+		
+		return Arrays.asList("Head First Java","Spring In Action","Thinking in java");
+		
+	}
+	@GetMapping(path = "/books/{id}")
+	public String getBookId(@PathVariable int id) {
+		
+		if(id==1) {
+			
+			return "Head First Java:"+port;
+			
+		}
+		else {
+			
+			return "Spring in Action:"+port;
+		}
+	}
+}
