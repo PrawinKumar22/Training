cd2d0455bb346a9f73f99f961ff61485f9f671ef
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-06-20 10:27:55 +0530

    spring mvc with jsp

--- spring-mvc-with-jsp/src/main/java/com/example/demo/SpringMvcWithJspApplication.java
+++ spring-mvc-with-jsp/src/main/java/com/example/demo/SpringMvcWithJspApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
