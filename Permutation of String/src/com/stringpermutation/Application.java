5dd9311f9bc3f7127e6647d6d39db15dc50304a3
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-08-10 17:43:22 +0530

    Permutation Of String

---
+++ Permutation of String/src/com/stringpermutation/Application.java
@@ -0,0 +1,13 @@
+package com.stringpermutation;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Permutation per = new Permutation();
+		
+		System.out.println(per);
+	}
+
+}
---
+++ Permutation of String/src/com/stringpermutation/Permutation.java
@@ -0,0 +1,30 @@
+package com.stringpermutation;
+
+public class Permutation {
+	
+	private String cat;
+
+	
+	public Permutation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Permutation(String cat) {
+		super();
+		this.cat = cat;
+	}
+
+	@Override
+	public String toString() {
+		return "Permutation [cat=" + cat + "]";
+	}
+	
+	
+
+
+
+
+
+	
+}
