437e9a460b474bfa8ebfe0dfb1181fda8f88f164
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-08-10 17:26:53 +0530

    microservicetwo

---
+++ microservice-two/src/main/java/com/example/demo/model/DriverDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverDTO {
+
+	private int driverId;
+	
+	private String driverName;
+	
+	private long phoneNumber;
+	
+	private double rating;
+}
