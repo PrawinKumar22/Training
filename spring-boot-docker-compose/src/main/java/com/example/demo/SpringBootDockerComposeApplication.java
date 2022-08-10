d48c4b532b1dd1e446eba40ceab104eceb560c1c
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-08-10 17:30:17 +0530

    springbootdockercompose

---
+++ spring-boot-docker-compose/src/main/java/com/example/demo/SpringBootDockerComposeApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.model.Product;
+
+@SpringBootApplication
+public class SpringBootDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerComposeApplication.class, args);
+	}
+
+	@Bean
+	public Product tv() {
+		
+		return new Product(102,"Sony Tv",26000);
+	}
+	
+	@Bean
+	public Product printer() {
+		
+		return new Product(103,"Cannon Printer",10000);
+
+	}
+}
+
