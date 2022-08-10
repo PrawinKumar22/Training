c1a6bb0ef30f5cc53d51af079c515e3124f5bbbe
Prawinkumar.B <Prawinkumar.B@EXIND3010-127.allfon.in>
2022-08-10 17:27:30 +0530

    rabbitmqconsumer

---
+++ rabbit-mq-consumer/src/main/java/com/example/demo/consumer/Consumer.java
@@ -0,0 +1,27 @@
+package com.example.demo.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.direct.config.DirectExchangeConfig;
+/**
+ * 
+ * @author Prawinkumar.B
+ *
+ */
+@Component
+public class Consumer {
+
+	/**
+	 * 
+	 * @param message
+	 */
+	@RabbitListener(queues = DirectExchangeConfig.ROUTING_KEY)
+	public void consumeMessage(String message) {
+	
+		System.out.println("Received One Message:="+message);
+
+	}
+	
+}
